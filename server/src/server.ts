215ade857b066bc929cf2cbcc308c653c03f30f7
Arthur Freitas Cesarino dos Santos <cesarinoArthur@gmail.com>
2022-08-13 11:06:15 -0300

    log error

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,6 +14,8 @@ app.use(router);
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
+        console.log(err);
+
         if (err instanceof AppError)
             return response
                 .status(err.statusCode)
